d0990de0f3487bb0bedc123f81c3ab8d8f976911
trantoan0199 <trantrungtoan9x@gmail.com>
2024-07-08 14:26:21 +0700

    Updates

---
+++ assets/user.api--ZsZSsJx.js
@@ -0,0 +1,1 @@
+import{D as r}from"./index-C2mn7hxx.js";const e={getProfile(){return r.get("me")},updateProfile(t){return r.put("user",t)},uploadAvatar(t){return r.post("user/upload-avatar",t,{headers:{"Content-Type":"multipart/form-data"}})}},u=e;export{u};
